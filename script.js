5eb7f416966ac4d76fe648368b4ff6a6877a34fd
Higor Gardin <higorgardin00@gmail.com>
2022-10-16 15:16:55 -0300

    Improved initialization

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+/**
+ * Tratamentos de inicialização
+ */
+function onInit() {
+  addListeners();
+  handleSettings();
+}
+
+/**
+ * Gerencia as configurações da extensão
+ */
 function handleSettings() {
   chrome.storage.sync.get(["isEnabled"], function (result) {
-    if (Boolean(result.isEnabled)) {
-      document.getElementById("enableDisable").checked = true;
-    } else {
-      document.getElementById("enableDisable").checked = false;
+    if (document && document.getElementById("enableDisable")) {
+      if (Boolean(result.isEnabled)) {
+        document.getElementById("enableDisable").checked = true;
+      } else {
+        document.getElementById("enableDisable").checked = false;
+      }
     }
   });
 }
 
+/**
+ * Adiciona os listeners
+ */
 function addListeners() {
   if (document && document.getElementById("enableDisable")) {
     document
@@ -19,5 +35,4 @@ function addListeners() {
   }
 }
 
-addListeners();
-handleSettings();
+onInit();
